a0cedd616f2b2284810810beaecc9daae0e873d7
Peter Ley <peteley11235@gmail.com>
2017-03-26 01:31:33 -0700

    ex1-8 counts blanks, tabs, and newlines

---
+++ 1-8.c
@@ -0,0 +1,29 @@
+/* Exercise 1-8:
+ * Write a program to count blanks, tabs and newlines.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+     int c;
+     int blanks, tabs, newlines;
+
+     blanks = 0;
+     tabs = 0;
+     newlines = 0;
+
+     while ((c = getchar()) != EOF)
+     {
+	  if (c == ' ')
+	       ++blanks;
+
+	  if (c == '\t')
+	       ++tabs;
+
+	  if (c == '\n')
+	       ++newlines;
+     }
+
+     printf("%d %d %d\n", blanks, tabs, newlines);
+}
