e7eac7b41f6e86c82a285e062735a7efe63b0b06
Peter Ley <peteley11235@gmail.com>
2017-03-26 02:15:18 -0700

    ex1-11 is answered in comments

---
+++ 1-11.c
@@ -0,0 +1,38 @@
+/* Exercise 1-11:
+ * How would you test the word count program? What kinds of input are
+ * most likely uncover bugs if there are any?
+ */
+
+/* 
+ * very long strings without spaces
+ * consecutive spaces
+ * lines wholly composed of spaces
+ * no input
+ * multiple empty lines
+ */
+
+#include <stdio.h>
+
+#define IN 1	/* inside a word */
+#define OUT 0	/* outside a word */
+
+/* count lines, words, and characters in input */
+int main()
+{
+     int c, nl, nw, nc, state;
+
+     state = OUT;
+     nl = nw = nc = 0;
+     while ((c = getchar()) != EOF) {
+	  ++nc;
+	  if (c == '\n')
+	       ++nl;
+	  if (c == ' ' || c = '\n' || c == '\t')
+	       state = OUT;
+	  else if (state == OUT) {
+	       state = IN;
+	       ++nw;
+	  }
+     }
+     printf("%d %d %d\n", nl, nw, nc);
+}
