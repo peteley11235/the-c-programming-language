c9bded7605729c31943edd0e5c92a69abe9a9901
Peter Ley <peteley11235@gmail.com>
2017-03-26 02:24:04 -0700

    minor bugfix ex1-11

--- 1-11.c
+++ 1-11.c
@@ -27,7 +27,7 @@ int main()
 	  ++nc;
 	  if (c == '\n')
 	       ++nl;
-	  if (c == ' ' || c = '\n' || c == '\t')
+	  if (c == ' ' || c == '\n' || c == '\t')
 	       state = OUT;
 	  else if (state == OUT) {
 	       state = IN;
